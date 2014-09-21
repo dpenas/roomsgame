6c59d896632d01c1d81428348e95343618488861
Dario Penas Sabin <dariosoyyo@gmail.com>
2014-09-21 18:20:25 +0200

    Finished roomsAvailable function

--- src/map/Map.java
+++ src/map/Map.java
@@ -5,11 +5,22 @@ import java.util.ArrayList;
 import util.RandUtil;
 import util.Tuple;
 
+/**
+ * TODO RELEVANT:
+ * - Complete initialize_rooms_map, which should call the other functions and create the actual map
+ß * 
+ * TODO ADDITIONAL: 
+ * - Map of maps? So we can teleport to other maps and stuff.
+ *
+ */
+
 public class Map {
-	
-	private ArrayList<Room> map;
+
 	private Tuple<Integer, Integer> global_init;
 	private Tuple<Integer, Integer> global_fin;
+	private int real_x;
+	private int real_y;
+	byte[][] free_room;
 	private int size;
 
 	/**
@@ -21,6 +32,11 @@ public class Map {
 	public Map(Tuple<Integer, Integer> global_init, Tuple<Integer, Integer> global_fin){
 		this.set_global_init(global_init);
 		this.set_global_init(global_fin);
+		real_x = this.global_fin().x - this.global_init().x;
+		real_y = this.global_fin().y - this.global_init().y;
+		size = real_x * real_y;
+		free_room = new byte[real_x][real_y];
+		initializeMatrixZero(free_room);
 	}
 	
 	/**
@@ -51,23 +67,49 @@ public class Map {
 	}
 	
 	/**
-)	 * Given the number of rooms and the dimension of the map, it sets
-	 * the map with its rooms
-	 * @return
+	 * 
+	 * @return Position of the next room that we must extend
 	 */
-	public ArrayList<Room> obtainRooms(){
-		int number_rooms = obtainNumberRooms();
+	public Tuple<Integer, Integer> obtainAvailableRooms(){
 		int real_x = this.global_fin().x - this.global_init().x;
 		int real_y = this.global_fin().y - this.global_init().y;
-		byte[][] free_room = new byte[real_x][real_y]; 
-		initializeMatrixZero(free_room);
-		
-		while (number_rooms != 0){
-			
+		ArrayList<Tuple<Integer, Integer>> possibleExtensionPoints = new ArrayList<>();
+		if (this.free_room[0][0] == 0){
+			possibleExtensionPoints.add(new Tuple<Integer, Integer>(0,0));
 		}
+		
+		for (int i = 0; i < real_x; i++){
+			for (int j = 0; j < real_y; j++){
+				if (i == 0 && j != 0){
+					if (free_room[0][j] == 0 && free_room[0][j-1] == 1){
+						possibleExtensionPoints.add(new Tuple<Integer, Integer>(0, j-1));
+					}
+				}
+				if (j == 0 && i != 0){
+					if (free_room[i][0] == 0 && free_room[i-1][0] == 1){
+						possibleExtensionPoints.add(new Tuple<Integer, Integer>(i-1, 0));
+					}
+				}
+				if ((free_room[i-1][j-1] == 1 && free_room[i][j] == 0)){
+					possibleExtensionPoints.add(new Tuple<Integer, Integer>(i-1, j-1));
+				}
+			}
+		}
+		
+		int position_random_selected = RandUtil.RandomNumber(0, possibleExtensionPoints.size());
+		
+		return possibleExtensionPoints.get(position_random_selected);
 
-		return new ArrayList<Room>();
 	}
+
+	/**
+	 * Main function that creates a map given its size, using the rest of the
+	 * class functions
+	 */
+	public void initialize_rooms_map(){
+		
+	}
+	
 	
 	public Tuple<Integer, Integer> global_init() {
 		return global_init;
