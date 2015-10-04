bac0db21499bb991ef916e52c2d39f45c53cd184
Darío <dariosoyyo@gmail.com>
2015-10-04 12:25:13 +0200

    Added the Dragon and Rat enemies

---
+++ src/characters/active/enemies/Dragon.java
@@ -0,0 +1,26 @@
+package characters.active.enemies;
+
+import items.Item;
+import items.wereables.WereableArmor;
+import items.wereables.WereableWeapon;
+
+import java.util.ArrayList;
+
+import characters.active.ActiveCharacter;
+import map.Map;
+import map.Room;
+import util.Tuple;
+
+public class Dragon extends ActiveCharacter {
+
+	public Dragon(Map map, Room room, Tuple<Integer, Integer> position, int inventorySpace,
+			ArrayList<Item> inventory) {
+		super("dragon", "", map, room, position, 8, 3, 50,
+				50, 500, 1000, new ArrayList<WereableWeapon>(), new ArrayList<WereableArmor>(), 0,
+				0, 0, inventory, 0,
+				0, 100, 0, 0, "D", 2, 2);
+	}
+	
+	
+
+}
---
+++ src/characters/active/enemies/Rat.java
@@ -0,0 +1,23 @@
+package characters.active.enemies;
+
+import items.Item;
+import items.wereables.WereableArmor;
+import items.wereables.WereableWeapon;
+
+import java.util.ArrayList;
+
+import characters.active.ActiveCharacter;
+import map.Map;
+import map.Room;
+import util.Tuple;
+
+public class Rat extends ActiveCharacter {
+
+	public Rat(Map map, Room room, Tuple<Integer, Integer> position, int inventorySpace,
+			ArrayList<Item> inventory) {
+		super("rat", "", map, room, position, 2, 1, 30,
+				50, 100, 100, new ArrayList<WereableWeapon>(), new ArrayList<WereableArmor>(), 0,
+				0, 0, inventory, 0,
+				0, 100, 0, 0, "G", 3, 2);
+	}
+}
